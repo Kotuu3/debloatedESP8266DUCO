b841d22226b832444837cad5197f2ffd47927cb0
Kotuu3 <kotuu303@proton.me>
2024-04-02 23:43:43 +0200

    Deleted sensors from code

--- Settings.h
+++ Settings.h
@@ -15,12 +15,7 @@ extern const char SSID[] = "";
 // Change the part in brackets to your WiFi password
 extern const char PASSWORD[] = "";
 
-// Uncomment if you want to host the dashboard page (available on ESPs IP address)
-// #define WEB_DASHBOARD
-
-// Uncomment if you want to use LAN8720. WLAN-credentials will be ignored using LAN
-// Select correct Board in ArduinoIDE!!! Really!
-// #define USE_LAN
+#define LED_HASHRATEWARNING
 
 // Comment out the line below if you wish to disable LED blinking
 #define LED_BLINKING
@@ -28,25 +23,8 @@ extern const char PASSWORD[] = "";
 // Comment out the line below if you wish to disable Serial printing
 #define SERIAL_PRINTING
 
-// -------------------------------------------------------------- //
-
-// ---------------------- IoT examples -------------------------- //
-// https://github.com/revoxhere/duino-coin/wiki/Duino's-take-on-the-Internet-of-Things
-
-// Uncomment the line below if you wish to use the internal temperature sensor (Duino IoT example)
-// Only ESP32-S2, -S3, -H2, -C2, -C3, -C6 and some old models have one!
-// More info: https://www.espboards.dev/blog/esp32-inbuilt-temperature-sensor/
-// NOTE: Mining performance will decrease by about 20 kH/s!
-// #define USE_INTERNAL_SENSOR
-
-// Uncomment the line below if you wish to use a DS18B20 temperature sensor (Duino IoT example)
-// NOTE: Mining performance should stay the same
-// #define USE_DS18B20
-
-// Uncomment the line below if you wish to use a DHT11/22 temperature and humidity sensor (Duino IoT example)
-// NOTE: Untested as of right now
-// #define USE_DHT
-// -------------------------------------------------------------- //
+#if defined(ESP8266)
+    #define LED_WARNING 6
 
 #if defined(ESP8266)
     // ESP8266
@@ -62,11 +40,6 @@ extern const char PASSWORD[] = "";
 #define BLINK_SETUP_COMPLETE 2
 #define BLINK_CLIENT_CONNECT 5
 
-
-
-
-
-
 #define SOFTWARE_VERSION "4.0"
 extern unsigned int hashrate = 0;
 extern unsigned int difficulty = 0;
@@ -74,30 +47,5 @@ extern unsigned long share_count = 0;
 extern String node_id = "";/
 extern String WALLET_ID = "";
 
-#if defined(USE_INTERNAL_SENSOR)
-  #include "driver/temp_sensor.h"
-#endif
-
-#if defined(USE_DS18B20)
-  // Install OneWire and DallasTemperature libraries if you get an error
-  #include <OneWire.h>
-  #include <DallasTemperature.h>
-  // Change 12 to the pin you've connected your sensor to
-  #define DSPIN 12
-  
-  OneWire oneWire(DSPIN);
-  DallasTemperature extern sensors(&oneWire);
-#endif
-
-#if defined(USE_DHT)
-  // Install "DHT sensor library" if you get an error
-  #include <DHT.h>
-  // Change 12 to the pin you've connected your sensor to
-  #define DHTPIN 12
-  // Set DHT11 or DHT22 type accordingly
-  #define DHTTYPE DHT11
-  
-  DHT extern dht(DHTPIN, DHTTYPE);
-#endif
 
 #endif  // End of SETTINGS_H
